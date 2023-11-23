a39c6a8c77d8515894371c460286e0aa5a29004e
murasawa <105776694+murasawa620@users.noreply.github.com>
2023-11-23 18:37:05 +0900

    Update encrypt.php

--- web/encrypt.php
+++ web/encrypt.php
@@ -3,6 +3,4 @@
     require_once(dirname(__FILE__).'/functions.php');
     require_once(dirname(__FILE__).'/../lib/crypt.php');
 
-    echo(encrypt("管理者", $secret_key, $secret_iv));
-    echo(decrypt("a2VFYmZtL2RVbjZVbERlb0tLVzRpQT09", $secret_key, $secret_iv));
 ?>
